c1ea51be901050f0636b7a23dbaae3fcd2f27477
Administrator <Administrator@DELL>
2020-05-14 20:45:57 +0530

    Initial commit.

---
+++ SpringCoreBeanLifeCycleVariousAwareInterfacesDemo/src/main/java/com/javalive/main/App.java
@@ -0,0 +1,19 @@
+package com.javalive.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.javalive.beans.AwareBean;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+		AwareBean bean = (AwareBean) context.getBean("awareBean");
+		((AbstractApplicationContext) context).registerShutdownHook();
+	}
+}
