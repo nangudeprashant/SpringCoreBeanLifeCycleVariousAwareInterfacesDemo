17d93da8b89316302aa975735d63b5869045c04b
Administrator <Administrator@DELL>
2020-07-20 17:39:13 +0530

    Project guide file is added.

--- SpringCoreBeanLifeCycleVariousAwareInterfacesDemo/src/main/java/com/javalive/beans/AwareBean.java
+++ SpringCoreBeanLifeCycleVariousAwareInterfacesDemo/src/main/java/com/javalive/beans/AwareBean.java
@@ -11,18 +11,18 @@ import org.springframework.context.ApplicationContextAware;
 
 public class AwareBean implements ApplicationContextAware,BeanNameAware,BeanFactoryAware{
 	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-	       System.out.println("setBeanFactory method of Aware bean is called");
+	       System.out.println("setBeanFactory method of BeanFactoryAware interface implemented by AwareBean class is called");
 	       System.out.println("setBeanFactory:: Aware bean singleton="
 	                + beanFactory.isSingleton("awareBean"));
 	}
 	public void setBeanName(String beanName) {
-	     System.out.println("setBeanName method of Aware bean is called");
+	     System.out.println("setBeanName method of BeanNameAware interface implemented by AwareBean class is called");
 	     System.out.println("setBeanName:: Bean Name defined in context="
 	                    + beanName);
 	}
 	public void setApplicationContext(ApplicationContext applicationContext)
 	        throws BeansException {
-	        System.out.println("setApplicationContext method of Aware bean is called");
+	        System.out.println("setApplicationContext method of ApplicationContextAware interface implemented by AwareBean class is called");
 	        System.out.println("setApplicationContext:: Bean Definition Names="
 	                + Arrays.toString(applicationContext.getBeanDefinitionNames()));
 	}
